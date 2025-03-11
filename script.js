8c52a33dfe3d20ffd28c09b2687f8a5b545721e4
Dev-VitorPereira <dev.vitorpereira@gmail.com>
2025-03-11 16:22:15 -0300

    feat:

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 document.getElementById("buttonHTML").addEventListener("click", function() {
-    window.open("./scr/HTML.html", "_blank"); // Abre em nova aba
+    window.open("./scr/HTML/html.html", "_blank"); // Abre em nova aba
 });
 
 document.getElementById("buttonCSS").addEventListener("click", function() {
-    window.open("./scr/css.html", "_blank"); // Abre em nova aba
+    window.open("./scr/CSS/css.html", "_blank"); // Abre em nova aba
 });
 
 document.getElementById("buttonJavaScript").addEventListener("click", function() {
-    window.open("./scr/javascript.html", "_blank"); // Abre em nova aba
+    window.open("./scr/JAVASCRIPT/javascript.html", "_blank"); // Abre em nova aba
 });
 
 document.getElementById("buttonJava").addEventListener("click", function() {
-    window.open("./scr/java.html", "_blank"); // Abre em nova aba
+    window.open("./scr/JAVA/java.html", "_blank"); // Abre em nova aba
 });
 
 document.getElementById("buttonPython").addEventListener("click", function() {
-    window.open("./scr/python.html", "_blank"); // Abre em nova aba
+    window.open("./scr/PYTHON/python.html", "_blank"); // Abre em nova aba
 });
