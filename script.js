1ded41faeb25f87a0521d0781699e6560cc8a5ae
Dev-VitorPereira <dev.vitorpereira@gmail.com>
2025-03-11 15:29:33 -0300

    feat: Trabalhando no estilo do button's

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById("buttonHTML").addEventListener("click", function() {
+    window.open("./scr/HTML.html", "_blank"); // Abre em nova aba
+});
+
+document.getElementById("buttonCSS").addEventListener("click", function() {
+    window.open("./scr/css.html", "_blank"); // Abre em nova aba
+});
+
+document.getElementById("buttonJavaScript").addEventListener("click", function() {
+    window.open("./scr/javascript.html", "_blank"); // Abre em nova aba
+});
+
+document.getElementById("buttonJava").addEventListener("click", function() {
+    window.open("./scr/java.html", "_blank"); // Abre em nova aba
+});
+
+document.getElementById("buttonPython").addEventListener("click", function() {
+    window.open("./scr/python.html", "_blank"); // Abre em nova aba
+});
