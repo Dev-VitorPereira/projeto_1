0c191976b2820f0aba419362838616aea7a8ce2a
Dev-VitorPereira <dev.vitorpereira@gmail.com>
2025-03-12 16:06:07 -0300

    feat: inserindo botao (mod escuro/claro)

--- script.js
+++ script.js
@@ -17,3 +17,17 @@ document.getElementById("buttonJava").addEventListener("click", function() {
 document.getElementById("buttonPython").addEventListener("click", function() {
     window.open("./scr/PYTHON/python.html", "_blank"); // Abre em nova aba
 });
+
+
+let isDark = false; // Variável para verificar o estado do tema
+
+document.getElementById("btn1").addEventListener("click", function() {
+    if (isDark) {
+        document.body.style.backgroundColor = ""; 
+        document.getElementById("btn1").textContent = "Modo Escuro"; // Altera o texto do botão
+    } else {
+        document.body.style.backgroundColor = "black"; // Cor escura
+        document.getElementById("btn1").textContent = "Modo Claro"; // Altera o texto do botão
+    }
+    isDark = !isDark; // Alterna o estado
+});
